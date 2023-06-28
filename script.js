8bae1654b0400c748a37364caa89ec2ae01d7997
Alex Ponomarev <ponomar3v.alexey@yandex.ru>
2023-06-28 17:16:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let photos = [];
+const input = document.querySelector('input');
+
+const getData = url => fetch(url)
+.then(res => res.json())
+.then(data => {
+    photos = data.hits;
+    showPhoto(photos);
+})
+.catch(error => console.error('Ошибка:', error))
+
+getData('https://pixabay.com/api/?key=37954840-02a2d9419214ddd959c05fe70&image_type=photo');
+
+function showPhoto (photos) {
+    const main = document.querySelector('.photo_container')
+    photos.forEach(elem => {
+        const photoItem = document.createElement('div');
+        photoItem.classList.add('photo_item');
+        photoItem.innerHTML = `
+            <h2>${elem.user}</h2>
+            <img src="${elem.largeImageURL}" alt="${elem.user}"></img>
+        `
+        main.appendChild(photoItem);
+    });
+}
+
+input.addEventListener('input', () => {
+
+})
