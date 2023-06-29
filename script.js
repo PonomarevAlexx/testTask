16a6d9e23b55c6417e880d2193de49f225edc0dd
Alex Ponomarev <ponomar3v.alexey@yandex.ru>
2023-06-29 16:54:14 +0300

    comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let photos = [];
 const input = document.querySelector('input');
 
+// Загрузка данных из API
 const getData = url => fetch(url)
 .then(res => res.json())
 .then(data => {
@@ -11,6 +12,7 @@ const getData = url => fetch(url)
 
 getData('https://pixabay.com/api/?key=37954840-02a2d9419214ddd959c05fe70&image_type=photo');
 
+// Функция вывода полученных фотографий на страницу
 function showPhoto (photos) {
     const main = document.querySelector('.photo_container')
     main.innerHTML = ``;
@@ -25,10 +27,12 @@ function showPhoto (photos) {
     });
 }
 
+// Функция фильтрации фотографий по тегу
 function filterPhotos() {
-    const valueFromInput = input.value;
-    const filtered = photos.filter(photo => photo.tags.indexOf(valueFromInput) > -1);
+    const valueFromInput = input.value.toUpperCase();
+    const filtered = photos.filter(photo => photo.tags.toUpperCase().indexOf(valueFromInput) > -1);
     showPhoto(filtered);
 }
 
+// Инпут для ввода тегов
 input.addEventListener('input', filterPhotos);
