5dadf0eb9a5b1e7d732209b3dab60f39c5f27b30
Alex Ponomarev <ponomar3v.alexey@yandex.ru>
2023-06-30 22:34:52 +0300

    end?

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let photos = [];
-let currentPage = 1;
+const URL_IMAGE_SERVER = 'https://pixabay.com/api/?key=37954840-02a2d9419214ddd959c05fe70&orientation=vertical&image_type=photo&per_page=21'
+
 const input = document.querySelector('.filter_input');
 const main = document.querySelector('.photo_container');
 const btnClearInput = document.querySelector('.btn_clear_input');
-const URL_IMAGE_SERVER = 'https://pixabay.com/api/?key=37954840-02a2d9419214ddd959c05fe70&orientation=vertical&image_type=photo&per_page=21'
 const btnPrevPage = document.querySelector('.btn_prev');
 const btnNextPage = document.querySelector('.btn_next');
 const pageNumber = document.querySelector('.number_page');
+
+let photos = [];
+let currentPage = 1;
 
 // Загрузка данных из API
 const getData = (url, page = 1) => fetch(`${url}&page=${page}`)
@@ -21,19 +23,18 @@ getData(URL_IMAGE_SERVER, currentPage);
 
 // Функция очистки фильтра тегов
 function clearInput() {
-    if(input.value !== ''){
         input.value = '';
         showPhoto(photos);
-    }
+        btnClearInput.setAttribute('disabled', true);
 }
 
 // Функция создающая элемент для вывода фото
-function createElementShowPhoto(elem){
+function createElementShowPhoto({user, largeImageURL}){
     const photoItem = document.createElement('div');
     photoItem.classList.add('photo_item');
     photoItem.innerHTML = `
-        <h2>${elem.user}</h2>
-        <img src="${elem.largeImageURL}" alt="${elem.user}"></img>
+        <h2>${user}</h2>
+        <img src="${largeImageURL}" alt="${user}"></img>
     `
     main.appendChild(photoItem);
 }
@@ -41,14 +42,16 @@ function createElementShowPhoto(elem){
 // Функция вывода полученных фотографий на страницу
 function showPhoto (photos) {
     main.innerHTML = ``;
-    photos.forEach(elem => createElementShowPhoto(elem));
+    photos.map(createElementShowPhoto);
 }
 
 // Функция фильтрации фотографий по тегу
 function filterPhotos() {
-    const valueFromInput = input.value.toUpperCase();
-    const filtered = photos.filter(photo => photo.tags.toUpperCase().indexOf(valueFromInput) > -1);
+    const valueFromInput = input.value.toUpperCase().trim();
+    const filtered = photos.filter(({tags}) => tags.toUpperCase().trim().includes(valueFromInput));
     showPhoto(filtered);
+
+    btnClearInput.disabled = !valueFromInput;
 }
 
 // Инпут для ввода тегов
@@ -57,18 +60,19 @@ input.addEventListener('input', filterPhotos);
 // Вызов функции очистки фильтра
 btnClearInput.addEventListener('click', clearInput);
 
-// Вызов предыдущей страницы
-btnPrevPage.addEventListener('click', () => {
-    currentPage--;
+// Разбиение на страницы
+function paginationControl(currentPage) {
     pageNumber.textContent = currentPage;
     getData(URL_IMAGE_SERVER, currentPage);
-    if(currentPage < 2) btnPrevPage.setAttribute('disabled', true);
+    btnPrevPage.disabled = currentPage < 2;
+}
+
+// Вызов предыдущей страницы
+btnPrevPage.addEventListener('click', () => {
+    paginationControl(--currentPage);
 });
 
 // Вызов следующей страницы
 btnNextPage.addEventListener('click', () => {
-    currentPage++;
-    pageNumber.textContent = currentPage;
-    getData(URL_IMAGE_SERVER, currentPage);
-    if(currentPage > 1) btnPrevPage.removeAttribute('disabled');
+    paginationControl(++currentPage);
 });
